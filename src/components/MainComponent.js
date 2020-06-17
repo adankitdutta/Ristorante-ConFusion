672619dd72bf91e5e8dac8e891e7cb09383246bc
Ankit Dutta <Ankit@LAPTOP-LF8HOQMD.(none)>
2020-06-17 21:14:20 +0530

    presentational and container

---
+++ src/components/MainComponent.js
@@ -0,0 +1,34 @@
+import React ,{Component} from 'react';
+import {Navbar,NavbarBrand} from 'reactstrap';
+import Menu from './MenuComponent';
+import {Dishes} from '../shared/dishes';
+import Dishdetail from './DishdetailComponent'
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: Dishes,
+            selectedDish: null
+        };
+      }
+    onSelectedDish(dishId) {
+        this.setState({
+            selectedDish: dishId
+        })
+    }
+  render(){
+  return (
+    <div>
+      <Navbar dark color='primary'>
+        <div className="container">
+          <NavbarBrand href="/">Restaurant Confusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu dishes={this.state.dishes} onClick={(dishId) => this.onSelectedDish(dishId)}/>
+      <Dishdetail dish={this.state.dishes.filter((dish)=>dish.id===this.state.selectedDish)[0]}/>
+    </div>
+  );
+}
+}
+export default Main;
