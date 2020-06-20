097249f840ea3a44bbb24e8be86fc596cb4e0c18
Ankit Dutta <Ankit@LAPTOP-LF8HOQMD.(none)>
2020-06-20 21:32:12 +0530

    intro to redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React,{Component} from 'react';
 import Main from './components/MainComponent';
 import {BrowserRouter} from 'react-router-dom';
 import './App.css';
-function App() {
+import {Provider} from 'react-redux';
+import {ConfigureStore} from './redux/configureStore';
 
+const store=ConfigureStore();
+class App extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
   return (
+    <Provider store={store}>
     <BrowserRouter>
     <div>
       <Main/>
     </div>
     </BrowserRouter>
+    </Provider>
   );
 }
-
+}
 export default App;
